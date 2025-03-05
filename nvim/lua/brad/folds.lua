f12a94aba3389c8bfce6a04656ac6d5c5278a036
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-03-05 12:12:33 -0600

    Better folds

--- nvim/lua/brad/folds.lua
+++ nvim/lua/brad/folds.lua
@@ -1,9 +1,14 @@
 return {
   'kevinhwang91/nvim-ufo',
-  dependencies = {'kevinhwang91/promise-async'},
-  opts = {},
+  dependencies = { 'kevinhwang91/promise-async', 'nvim-treesitter/nvim-treesitter' },
+  event = 'BufReadPost',
+  opts = {
+    provider_selector = function()
+      return { 'treesitter', 'indent' }
+    end,
+  },
   keys = {
-    {'zR', "lua=require('ufo').openAllFolds", desc = 'Open All Folds'},
-    {'zM', "lua=require('ufo').closeAllFolds", desc = 'Close All Folds'}
-  }
+    { 'zR', "lua=require('ufo').openAllFolds", desc = 'Open All Folds' },
+    { 'zM', "lua=require('ufo').closeAllFolds", desc = 'Close All Folds' },
+  },
 }
--- nvim/lua/sets/set.lua
+++ nvim/lua/sets/set.lua
@@ -8,7 +8,7 @@ vim.opt.cursorline = true
 vim.opt.mouse = 'nv'
 
 -- Folds
-vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldcolumn = 'auto:9' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
@@ -16,7 +16,7 @@ vim.opt.fillchars = {
   foldopen = '',
   foldclose = '',
   fold = ' ',
-  foldsep = ' ',
+  foldsep = '│',
   diff = '╱',
   eob = ' ',
 }
