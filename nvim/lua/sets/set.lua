43f201818b65feab993541a956ee05a3a2a7f1e4
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-04-22 13:05:19 -0500

    qol changes

--- nvim/lua/sets/set.lua
+++ nvim/lua/sets/set.lua
@@ -3,32 +3,18 @@ vim.loader.enable()
 -- For Obsidian
 vim.opt.conceallevel = 1
 
-vim.opt.guicursor = ''
+vim.opt.guicursor = 'n-v-c-sm:block-Cursor,i-ci-ve:ver35-Cursor,r-cr-o:hor30-Cursor'
 vim.opt.cursorline = true
 vim.opt.mouse = 'nv'
-
--- Folds
-vim.o.foldcolumn = 'auto:9' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-vim.opt.fillchars = {
-  foldopen = '',
-  foldclose = '',
-  fold = ' ',
-  foldsep = '│',
-  diff = '╱',
-  eob = ' ',
-}
 
 vim.opt.nu = true
 vim.opt.rnu = true
 
 vim.g.netrw_banner = 0
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.smarttab = true
