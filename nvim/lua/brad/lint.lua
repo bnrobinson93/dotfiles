bd8a71213e76bfd2c117a37edfbde9851f71c89c
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-05-02 12:23:13 -0500

    fixes for lsp and lint

--- nvim/lua/brad/lint.lua
+++ nvim/lua/brad/lint.lua
@@ -1,12 +1,14 @@
 return {
   'mfussenegger/nvim-lint',
-  setup = function()
+  config = function()
     require('lint').linters_by_ft = {
       markdown = { 'vale' },
       typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
       javascript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
+      yaml = { 'actionlint' },
+      ['yaml.ghaction'] = { 'actionlint' },
     }
 
     vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
@@ -15,5 +17,15 @@ return {
         require('lint').try_lint 'cspell'
       end,
     })
+
+    vim.api.nvim_create_user_command('LintInfo', function()
+      local filetype = vim.bo.filetype
+      local linters = require('lint').linters_by_ft[filetype]
+      if linters then
+        print('Linters for ' .. filetype .. ': ' .. table.concat(linters, ', '))
+      else
+        print('No linters configured for filetype: ' .. filetype)
+      end
+    end, {})
   end,
 }
