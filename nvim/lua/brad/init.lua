a799b057e89866440b71c9134b4e9f6da3153ebf
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-05-05 17:39:46 -0500

    markdown updates

--- nvim/lua/brad/init.lua
+++ nvim/lua/brad/init.lua
@@ -1,4 +1,27 @@
 return {
   'mbbill/undotree', -- tre view of history
   'stevearc/dressing.nvim', -- Better select and insert boxes
+  {
+    'preservim/vim-pencil', -- For writing
+    ft = { 'markdown', 'mkd', 'text' },
+    config = function()
+      vim.g['pincel#wrapModeDefault'] = 'soft'
+      vim.fn['pencil#init']()
+    end,
+  },
+  {
+    'lervag/vimtex', -- LaTeX support
+    ft = { 'tex' },
+    config = function()
+      require 'plugins.vimtex'
+    end,
+  },
+  {
+    'iamcco/markdown-preview.nvim', -- Markdown preview
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 }
