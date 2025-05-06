5ad536090e71b7616bd929b660b0464b77edd554
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-05-06 12:57:54 -0500

    markdown updates

--- nvim/lua/brad/markdown.lua
+++ nvim/lua/brad/markdown.lua
@@ -1,23 +1,24 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  ft = { 'markdown', 'Avante' },
+  ft = { 'markdown', 'Avante', 'vimwiki', 'help' },
   opts = {
-    render_modes = { 'n' },
+    render_modes = { 'n', 'c', 't' },
     checkbox = {
       unchecked = { icon = ' ' },
       checked = { icon = '󰄳 ', scope_highlight = '@markup.strikethrough' },
       custom = {
-        todo = { raw = '[-]', rendered = '󰍷 ', highlight = 'DiagnosticError' },
-        important = { raw = '[!]', rendered = ' ', highlight = 'DiagnosticWarn' },
-        partial = { raw = '[~]', rendered = ' ', highlight = 'DiagnosticInfo' },
-        delayed = { raw = '[>]', rendered = '󰥔 ', highlight = 'DiagnosticWarn' },
-        partial_parent = { raw = '[/]', rendered = '󰁊 ', highlight = 'SpecialKey', scope_highlight = '@markup.strikethrough' },
+        todo = { raw = '[-]', rendered = '󰍷 ', highlight = 'DiagnosticSignError' },
+        important = { raw = '[!]', rendered = ' ', highlight = 'DiagnosticSignWarn' },
+        partial = { raw = '[~]', rendered = ' ', highlight = 'DiagnosticSignHint' },
+        delayed = { raw = '[>]', rendered = '󰥔 ', highlight = 'DiagnosticSignWarn' },
+        partial_parent = { raw = '[/]', rendered = '󰁊 ', highlight = 'DiagnosticDepricated', scope_highlight = '@markup.strikethrough' },
       },
     },
     pipe_table = {
       enabled = true,
       cell = 'trimmed',
+      preset = 'round',
     },
   },
 }
