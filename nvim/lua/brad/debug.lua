eaab8b46d2f89097d2f19b2bc51502e42fe26079
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2025-05-14 23:21:50 -0500

    getting there

--- nvim/lua/brad/debug.lua
+++ nvim/lua/brad/debug.lua
@@ -7,11 +7,74 @@ local js_based_languages = {
 }
 
 return {
+  -- dap
   {
     'mfussenegger/nvim-dap',
+    recommended = true,
+    desc = 'Debugging support. Requires language specific adapters to be configured. (see lang extras)',
+
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      { 'microsoft/vscode-js-debug', build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out' },
+      {
+        'mxsdev/nvim-dap-vscode-js',
+        config = function()
+          require('dap-vscode-js').setup {
+            debugger_path = vim.fn.resolve(vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug'),
+            adapaters = {
+              'chrome',
+              'firefox',
+              'pwa-node',
+              'pwa-chrome',
+              'pwa-msedge',
+              'pwa-extensionHost',
+              'node-terminal',
+              'node',
+            },
+          }
+        end,
+      },
+      {
+        'Joakker/lua-json5',
+        build = './install.sh',
+      },
+    },
+
+  -- stylua: ignore
+  keys = {
+    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
+    { "<leader>dA", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
+    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+  },
+
     config = function()
       local dap = require 'dap'
 
+      vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
+
+      vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' })
+      vim.fn.sign_define('DapStopped', { text = '', texthl = 'DiagnosticSignWarn', linehl = '', numhl = '' })
+
+      -- setup dap config by VsCode launch.json file
+      local vscode = require 'dap.ext.vscode'
+      local json = require 'plenary.json'
+      vscode.json_decode = function(str)
+        return vim.json.decode(json.json_strip_comments(str))
+      end
       for _, language in ipairs(js_based_languages) do
         dap.configurations[language] = {
           {
@@ -31,6 +94,7 @@ return {
             cwd = '${workspaceFolder}',
             sourceMaps = true,
           },
+          -- Debug web apps (client side)
           -- Debug web apps (client side)
           {
             type = 'pwa-chrome',
@@ -56,6 +120,94 @@ return {
             protocol = 'inspector',
             sourceMaps = true,
             userDataDir = false,
+            -- Be explicit about Chrome path based on OS
+            runtimeExecutable = function()
+              -- Find Chrome path based on OS
+              if vim.fn.has 'mac' == 1 then
+                -- macOS
+                local chrome_paths = {
+                  '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+                  '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+                  -- Add other possible paths
+                }
+                for _, path in ipairs(chrome_paths) do
+                  if vim.fn.filereadable(path) == 1 then
+                    return path
+                  end
+                end
+              elseif vim.fn.has 'unix' == 1 then
+                -- Linux
+                local chrome_paths = {
+                  '/usr/bin/google-chrome',
+                  '/usr/bin/google-chrome-stable',
+                  '/usr/bin/chromium',
+                  '/usr/bin/chromium-browser',
+                  -- Add other possible paths
+                }
+                for _, path in ipairs(chrome_paths) do
+                  if vim.fn.filereadable(path) == 1 then
+                    return path
+                  end
+                end
+              elseif vim.fn.has 'win32' == 1 then
+                -- Windows
+                local chrome_paths = {
+                  'C:/Program Files/Google/Chrome/Application/chrome.exe',
+                  'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+                  -- Add other possible paths
+                }
+                for _, path in ipairs(chrome_paths) do
+                  if vim.fn.filereadable(path) == 1 then
+                    return path
+                  end
+                end
+              end
+
+              -- Default return if no Chrome found
+              return 'chrome' -- This will use the system's default 'chrome' command
+            end,
+            -- Launch Chrome with remote debugging enabled
+            runtimeArgs = {
+              '--remote-debugging-port=9222',
+              '--user-data-dir=${workspaceFolder}/.vscode/chrome-debug-profile',
+              '--no-first-run',
+            },
+            port = 9222,
+          },
+
+          -- Debug web apps (client side) in default browser
+          {
+            type = 'pwa-node',
+            request = 'launch',
+            name = 'Launch Default Browser',
+            program = function()
+              local url = vim.fn.input('Enter URL: ', 'http://localhost:3000')
+
+              -- Open browser before starting debug session
+              local open_cmd = ''
+              if vim.fn.has 'mac' == 1 then
+                open_cmd = 'open'
+              elseif vim.fn.has 'unix' == 1 then
+                open_cmd = 'xdg-open'
+              elseif vim.fn.has 'win32' == 1 then
+                open_cmd = 'start'
+              end
+
+              vim.fn.jobstart(open_cmd .. ' ' .. url)
+
+              -- Return a small script that keeps the debug session alive
+              local temp_script = os.tmpname() .. '.js'
+              local f = io.open(temp_script, 'w')
+              f:write 'console.log("Browser debug session started."); setTimeout(() => {}, 999999);'
+              f:close()
+
+              return temp_script
+            end,
+            cwd = '${workspaceFolder}',
+            sourceMaps = true,
+            protocol = 'inspector',
+            console = 'integratedTerminal',
+            skipFiles = { '<node_internals>/**' },
           },
 
           -- Devider for launch.json derived configs
@@ -67,82 +219,81 @@ return {
         }
       end
     end,
-    keys = {
-      {
-        '<leader>db',
-        function()
-          require('dap').toggle_breakpoint()
-        end,
-        desc = '[D]ebug [B]reakpoint',
-      },
-      {
-        '<leader>dc',
-        function()
-          require('dap').continue()
-        end,
-        desc = '[D]ebug [C]ontinue',
-      },
-      {
-        '<leader>di',
-        function()
-          require('dap').step_into()
-        end,
-        desc = '[D]ebug [I]nto',
-      },
-      {
-        '<leader>do',
-        function()
-          require('dap').step_over()
-        end,
-        desc = '[D]ebug [O]ver',
-      },
-      {
-        '<leader>dO',
-        function()
-          require('dap').step_out()
-        end,
-        desc = '[D]ebug Step [O]ut',
-      },
-      {
-        '<leader>dA',
-        function()
-          if vim.fn.filereadable '.vscode/launch.json' then
-            local dap_vscode = require 'dap.ext.vscode'
-            dap_vscode.load_launchjs(nil, {
-              ['pwa-node'] = js_based_languages,
-              ['node'] = js_based_languages,
-              ['chrome'] = js_based_languages,
-              ['pwa-chrome'] = js_based_languages,
-            })
-          else
-            vim.notify 'No launch.json found'
-          end
-          require('dap').continue()
-        end,
-        desc = '[D]ebug with [A]rguments',
+  },
+  -- virtual text
+  {
+    'theHamsta/nvim-dap-virtual-text',
+    opts = {},
+  },
+  -- nvim dap ui
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'nvim-neotest/nvim-nio' },
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+  },
+    opts = {},
+    config = function(_, opts)
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close {}
+      end
+    end,
+  },
+  { 'nvim-neotest/nvim-nio' },
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    dependencies = 'mason.nvim',
+    cmd = { 'DapInstall', 'DapUninstall' },
+    opts = {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
       },
     },
+    -- mason-nvim-dap is loaded when nvim-dap loads
+    config = function() end,
+  },
+
+  -- mason
+  {
+    'jay-babu/mason-nvim-dap.nvim',
     dependencies = {
-      { 'microsoft/vscode-js-debug', build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out' },
-      {
-        'mxsdev/nvim-dap-vscode-js',
-        opts = {
-          debugger_path = vim.fn.resolve(vim.fn.stdpath 'data' .. '/lazy/vscode-js-debug'),
-          adapaters = {
-            'chrome',
-            'pwa-node',
-            'pwa-chrome',
-            'pwa-msedge',
-            'pwa-extensionHost',
-            'node-terminal',
-            'node',
-          },
-        },
-      },
-      {
-        'Joakker/lua-json5',
-        build = './install.sh',
-      },
+      'williamboman/mason.nvim',
+      'mfussenegger/nvim-dap',
     },
+    config = function()
+      require('mason-nvim-dap').setup {
+        -- Automatically install these adapters
+        ensure_installed = {
+          'python', -- For Python debugging
+          'node2', -- For JavaScript/TypeScript
+          'codelldb', -- For C, C++, Rust
+          'delve', -- For Go
+          'firefox',
+        },
+        -- Automatically set up adapter configurations
+        automatic_setup = true,
+      }
+    end,
   },
 }
