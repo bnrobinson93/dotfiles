ca86b612ac7f6f1c614316f8662fbef04c5403f6
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-07-22 16:01:03 -0500

    add jj stuff for nvim

--- nvim/lua/brad/jj.lua
+++ nvim/lua/brad/jj.lua
@@ -3,8 +3,13 @@ return {
   {
     'julienvincent/hunk.nvim',
     cmd = { 'DiffEditor' },
+    cond = require('telescope.utils').get_os_command_output { 'git', 'rev-parse', '--is-inside-work-tree' },
     config = function()
       require('hunk').setup()
     end,
+    keys = {
+      { '<leader>gf', '<cmd>diffget //2<cr>', desc = 'Git diffget 2 (left hand)' },
+      { '<leader>gj', '<cmd>diffget //3<cr>', desc = 'Git diffget 3 (right hand)' },
+    },
   },
 }
