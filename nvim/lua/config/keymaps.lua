215e6c9c6941d05ce3c72d223a6e373c4a5872f9
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-07-29 15:42:39 -0500

    remove keymap for H and L

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+vim.keymap.del("n", "H")
+vim.keymap.del("n", "L")
 
 local map = vim.keymap.set
 
