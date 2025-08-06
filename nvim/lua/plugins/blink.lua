57e9aeb0e559b539d01e80369176df8037e84916
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2025-08-06 01:42:30 -0500

    fix super tab

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -11,12 +11,14 @@ return {
 
   {
     "saghen/blink.cmp",
-    keymap = {
-      preset = "super-tab",
-      ["<Tab>"] = {
-        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
-        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
-        "fallback",
+    opts = {
+      keymap = {
+        preset = "super-tab",
+        ["<Tab>"] = {
+          require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+          require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        },
       },
     },
   },
