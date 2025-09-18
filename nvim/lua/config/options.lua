8a3e6f4ac0f7c7357ccb1c49247c3dbe1810cb41
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-09-18 12:47:46 -0500

    disable hints (#20)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 vim.opt.clipboard = ""
 vim.opt.confirm = false
+vim.lsp.inlay_hint.enable(false)
 
 local opt = vim.opt
 
