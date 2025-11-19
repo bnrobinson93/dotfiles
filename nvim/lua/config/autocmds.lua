19652ef49bbdf6c2d28b581013fe82098eb9cc81
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2025-11-19 11:26:15 -0600

    fix lint (#71)
    * fix lint
    
    * suggestions

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -19,7 +19,6 @@ augroup END
 ]])
 
 -- YAML
-local lint = pcall(require("lint"))
 autocmd({ "BufRead", "BufNewFile" }, {
   pattern = { "*.yaml", "*.yml" },
   desc = "Highlight trailing whitespace in YAML files",
@@ -71,13 +70,12 @@ autocmd({ "BufRead", "BufNewFile" }, {
 
     update_diagnostics()
 
-    local timer = vim.loop.new_timer()
+    local timer
     autocmd("TextChanged", {
       buffer = bufnr,
       callback = function()
-        timer:start(
+        timer = vim.fn.timer_start(
           500,
-          0,
           vim.schedule_wrap(function()
             if vim.api.nvim_buf_is_valid(bufnr) then
               update_diagnostics()
@@ -90,7 +88,7 @@ autocmd({ "BufRead", "BufNewFile" }, {
     autocmd("BufDelete", {
       buffer = bufnr,
       callback = function()
-        timer:close()
+        vim.fn.timer_stop(timer)
       end,
     })
   end,
