17c32e618b9937a4a2f78a69ffb70947f414a123
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2025-12-08 15:11:48 -0600

    atuin

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -11,7 +11,8 @@ set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub)
 
 
 # Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
-fzf_configure_bindings
+# Disable history binding to allow Atuin to use Ctrl+R
+fzf_configure_bindings --history=
 
 # Doesn't erase autoloaded _fzf_* functions because they are not easily accessible once key bindings are erased
 function _fzf_uninstall --on-event fzf_uninstall
