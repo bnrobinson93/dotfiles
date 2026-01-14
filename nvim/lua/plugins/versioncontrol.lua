54227e790fff37f046c1d4701f3bb686b69efde8
Brad Robinson <31802085+bnrobinson93@users.noreply.github.com>
2026-01-14 16:25:00 -0600

    JJ improvements (#98)
    * jj.nvim
    
    * starship jj
    
    * allow diff review in nvim

--- nvim/lua/plugins/versioncontrol.lua
+++ nvim/lua/plugins/versioncontrol.lua
@@ -1,63 +1,80 @@
 return {
-  "julienvincent/hunk.nvim",
-  cmd = { "DiffEditor" },
-  opts = {
-    keys = {
-      global = {
-        quit = { "q" },
-        accept = { "<leader><Cr>" },
-        focus_tree = { "<leader>e" },
+  {
+    "NicolasGB/jj.nvim",
+    version = "*",
+    cond = function()
+      if vim.fn.executable("jj") == 0 then
+        return false
+      end
+      local root = vim.fn.system({ "jj", "root" })
+      if vim.v.shell_error ~= 0 or root == "" then
+        return false
+      end
+      return true
+    end,
+    opts = {},
+  },
+  {
+    "julienvincent/hunk.nvim",
+    cmd = { "DiffEditor" },
+    opts = {
+      keys = {
+        global = {
+          quit = { "q" },
+          accept = { "<leader><Cr>" },
+          focus_tree = { "<leader>e" },
+        },
+
+        tree = {
+          expand_node = { "l", "<Right>" },
+          collapse_node = { "h", "<Left>" },
+
+          open_file = { "<Cr>" },
+
+          toggle_file = { "a" },
+        },
+
+        diff = {
+          toggle_hunk = { "A" },
+          toggle_line = { "a" },
+          -- This is like toggle_line but it will also toggle the line on the other
+          -- 'side' of the diff.
+          toggle_line_pair = { "s" },
+
+          prev_hunk = { "[h" },
+          next_hunk = { "]h" },
+
+          -- Jump between the left and right diff view
+          toggle_focus = { "<Tab>" },
+        },
       },
 
-      tree = {
-        expand_node = { "l", "<Right>" },
-        collapse_node = { "h", "<Left>" },
-
-        open_file = { "<Cr>" },
-
-        toggle_file = { "a" },
+      ui = {
+        tree = {
+          -- Mode can either be `nested` or `flat`
+          mode = "nested",
+          width = 35,
+        },
+        --- Can be either `vertical` or `horizontal`
+        layout = "vertical",
       },
 
-      diff = {
-        toggle_hunk = { "A" },
-        toggle_line = { "a" },
-        -- This is like toggle_line but it will also toggle the line on the other
-        -- 'side' of the diff.
-        toggle_line_pair = { "s" },
+      icons = {
+        selected = "",
+        deselected = "",
+        partially_selected = "",
 
-        prev_hunk = { "[h" },
-        next_hunk = { "]h" },
-
-        -- Jump between the left and right diff view
-        toggle_focus = { "<Tab>" },
+        folder_open = "",
+        folder_closed = "",
       },
-    },
 
-    ui = {
-      tree = {
-        -- Mode can either be `nested` or `flat`
-        mode = "nested",
-        width = 35,
+      -- Called right after each window and buffer are created.
+      hooks = {
+        ---@param _context { buf: number, tree: NuiTree, opts: table }
+        on_tree_mount = function(_context) end,
+        ---@param _context { buf: number, win: number }
+        on_diff_mount = function(_context) end,
       },
-      --- Can be either `vertical` or `horizontal`
-      layout = "vertical",
-    },
-
-    icons = {
-      selected = "",
-      deselected = "",
-      partially_selected = "",
-
-      folder_open = "",
-      folder_closed = "",
-    },
-
-    -- Called right after each window and buffer are created.
-    hooks = {
-      ---@param _context { buf: number, tree: NuiTree, opts: table }
-      on_tree_mount = function(_context) end,
-      ---@param _context { buf: number, win: number }
-      on_diff_mount = function(_context) end,
     },
   },
 }
