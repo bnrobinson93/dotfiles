a7927db4812025e2b18a185fd43c7573b05f65df
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2026-02-27 09:44:23 -0600

    hover for info harper

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,6 +4,14 @@ return {
     servers = {
       harper_ls = {
         filetypes = { "markdown", "text" },
+        on_init = function(client, _)
+          -- Hide INFO diagnostics for this server only (can still hover)
+          vim.diagnostic.config({
+            virtual_text = {
+              severity = { min = vim.diagnostic.severity.WARN },
+            },
+          }, vim.lsp.diagnostic.get_namespace(client.id))
+        end,
         settings = {
           ["harper-ls"] = {
             userDictPath = vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
