7a4fc8693151df607a746956f9052b80b3226a2a
Brad R <31802085+bnrobinson93@users.noreply.github.com>
2026-02-28 00:19:48 -0600

    swap mappings to nvim-surround

---
+++ nvim/lua/plugins/surround.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "cs",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "ys", group = "surround > add" },
+        { "ds", group = "surround > delete" },
+        { "cs", group = "surround > change" },
+      },
+    },
+  },
+}
